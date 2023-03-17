f2dda3e6019ea6f4ec9b3ea9da3065f003757a8e
Nguyễn Mạnh Dũng Ph 1 8 8 3 8 <dungnmph18838@fpt.edu.vn>
2023-03-17 19:52:50 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,57 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const http = require('http');
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://admin:admin@cluster0.yw7opqt.mongodb.net/test').then((error) => {
-    if (error == null) console.log("Connect Success")
+const http = require("http");
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb+srv://admin:admin@cluster0.yw7opqt.mongodb.net/test")
+  .then((error) => {
+    if (error == null) console.log("Connect Success");
+  });
+const account = new mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  fullname: String,
 });
-const wallpaper = new mongoose.Schema({
-    id: String,
-    name: String,
-    mota: String,
-    linkAnhThuNho: String,
-    linkAnhGoc: String
-})
+const taikhoan = mongoose.model("Account", account);
 
-
-router.get('/', function (req, res, next) {
-    const hinhnen = mongoose.model('wallpapers', wallpaper);
-    hinhnen.find({}).then(data => {
-        if (data.length <= 20) {
-            res.render('index', {list: data, a: 0, b: data.length, title: 1})
-        }
-        res.render('index', {list: data, a: 0, b: 20, title: 1})
-    })
-});
-router.post('/nextpage', function (req, res, next) {
-    var title = parseFloat(req.body.title)
-    var a = parseFloat(req.body.a)
-    var b = parseFloat(req.body.b)
-    const hinhnen = mongoose.model('wallpapers', wallpaper);
-    hinhnen.find({}).then(data => {
-        if (b < data.length) {
-            if (20 + b > data.length) {
-                res.render('index', {list: data, a: b, b: data.length, title: title + 1})
-            } else {
-                res.render('index', {list: data, a: b, b: b + 20, title: title + 1})
-            }
-        } else {
-            console.log("vào đây" + 3)
-            res.render('index', {list: data, a: a, b: b, title: title})
-        }
-
-    })
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("error");
 });
 
-router.get('/sendreq', function (req, res, next) {
-    console.log("đang lấy data")
-    const hinhnen = mongoose.model('wallpapers', wallpaper);
-    hinhnen.find({}).then(data => {
-        res.send(data)
-    })
-
-
+router.post("/login", function (req, res, next) {});
+router.post("/register", function (req, res, next) {
+  var data = req.body;
+  var reply = { replyusername: true, replyemail: true };
+  taikhoan.find({ username: data.username }).then((data) => {
+    if (data.length > 0) {
+      reply.replyusername = false;
+    }
+  });
+  taikhoan.find({ email: data.email }).then((data) => {
+    if (data.length > 0) {
+      reply.replyemail = false;
+    }
+  });
+  if (reply.replyusername && reply.replyemail) {
+    var tkmoi = new taikhoan({
+      username: data.username,
+      password: data.password,
+      email: data.email,
+      fullname: data.fullname,
+    });
+    tkmoi.save((err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Đăng ký thành công");
+      }
+    });
+  } else {
+    console.log("Đăng ký thất bại do trùng tên hoặc email");
+  }
+  res.send(reply);
 });
 
 module.exports = router;
